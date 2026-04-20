3b0d4ab13e63ec73244d8c141340d4d96296092e
NikasNikasNikas <nikas.jxd@gmail.com>
2026-04-20 19:02:20 +0300

    Detached7 (#4)
    * Hopefully working basic page that can add books.
    
    * Allows to add categories, refresh so it is shown on ui.
    
    * Added category tables that show current books with specific categories.
    
    * See whether this commits.
    
    * Fixed some styling in frontend. Moved some methods from controller to services so it isnt as cluttered. Cut some superflous methods from services. Also added some verification in dtos.
    
    * Made frontend ui changes.
    
    * Added a DAO wrapper for repositories of book entity and service.
    
    * Added a DAO wrapper for repositories of book entity and service.
    
    * Added https request scope to controller.

--- src/main/java/com/example/library/controller/AuthorController.java
+++ src/main/java/com/example/library/controller/AuthorController.java
@@ -3,9 +3,11 @@ package com.example.library.controller;
 import com.example.library.dto.AuthorResponseDTO;
 import com.example.library.service.AuthorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Scope("request")
 @RestController
 @RequestMapping("/api/authors")
 @CrossOrigin(origins = "http://localhost:5173")
